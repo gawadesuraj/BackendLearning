58aad4f5b1dbe633ea4422780f203ba830c22693
gawadesuraj <csemr.surajgawade@gmail.com>
2026-03-26 21:41:08 +0530

    file updated

--- module/auth/auth.controller.js
+++ module/auth/auth.controller.js
@@ -1,9 +1,9 @@
 import * as authSevice from "./auth.service.js"
 import ApiResponse from "../../common/utils/api-response.js"
 
-const resister = async (req, res) => {
+const register = async (req, res) => {
   const user = await authSevice.register(req.body);
   ApiResponse.created(res, "Registration Success", user);
 };
 
-export { resister };
+export { register };
